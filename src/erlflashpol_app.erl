c91c3fe80fe0251c74fddf4d8c84781ca706a523
Hunter Morris <hunter.morris@smarkets.com>
2011-06-06 20:02:09 +0100

    Read configuration from env

--- src/erlflashpol_app.erl
+++ src/erlflashpol_app.erl
@@ -7,7 +7,14 @@
 start() -> application:start(erlflashpol).
 
 start(_StartType, _StartArgs) ->
-    % Default to port 8843 and liberal policy file
-    erlflashpol_sup:start_link(any, 8843, liberal).
+    Config =
+        case application:get_all_env(erlflashpol) of
+            undefined -> [];
+            L -> L
+        end,
+    ListenIp = proplists:get_value(listen_ip, Config, any),
+    ListenPort = proplists:get_value(listen_port, Config, 8843),
+    PolicyFile = proplists:get_value(policy_file, Config, liberal),
+    erlflashpol_sup:start_link(ListenIp, ListenPort, PolicyFile).
 
 stop(_State) -> ok.
